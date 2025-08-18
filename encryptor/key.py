5c8c00494a7a5242506f2000518567a9f798e029
msuL9 <msu9@uci.edu>
2025-08-18 11:17:43 -0700

    adding

---
+++ encryptor/key.py
@@ -0,0 +1,17 @@
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.backends import default_backend
+import os
+
+def derive_key(password: str, salt: bytes = None) -> tuple[bytes, bytes]:
+    if salt is None:
+        salt = os.urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    key = kdf.derive(password.encode())
+    return key, salt
